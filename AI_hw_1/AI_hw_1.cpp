ecbabb883f45d40b56cfdfb584bb4fd7c5ab20bf
Tonyvitamin <lai30942.cs04@nctu.edu.tw>
2018-03-16 09:45:34 +0800

    modified:   AI_hw_1.cpp 	modified:   a.out

--- AI_hw_1/AI_hw_1.cpp
+++ AI_hw_1/AI_hw_1.cpp
@@ -11,10 +11,75 @@
 using namespace std;
 
 
-void BFS(int d){
+void BFS(deque<int> steps, int x, int y){
+    deque<int> xpos;
+    deque<int> ypos;
+    xpos.push_back(0);
+    ypos.push_back(0);
+    while(!steps.empty()){
+        int cur_x , cur_y;
+        int next_step = steps.front();
+        steps.pop_front();
+        deque<int> tempx , tempy;
+        cout<<next_step<<endl;
+        while(!xpos.empty()){
+            // +x 
+            cur_x = xpos.front() + next_step;
+            cur_y = ypos.front();
+            tempx.push_back(cur_x);
+            tempy.push_back(cur_y);
+            cout<<"after +x "<< cur_x << " "<<cur_y<<endl;
+            //cout<<"after +x "<< xpos.back() << " "<<ypos.back()<<endl;
+            if(x==cur_x && y==cur_y)
+                return;
+            //+y
+            cur_x = xpos.front();
+            cur_y = ypos.front() + next_step;
+            tempx.push_back(cur_x);
+            tempy.push_back(cur_y);
+            cout<<"after +y "<< cur_x << " "<<cur_y<<endl;
+            if(x==cur_x && y==cur_y)
+                return;     
+            //-x
+            cur_x = xpos.front() - next_step;
+            cur_y = ypos.front();
+            tempx.push_back(cur_x);
+            tempy.push_back(cur_y);
+            cout<<"after -x "<< cur_x << " "<<cur_y<<endl;
+            if(x==cur_x && y==cur_y)
+                return; 
+            //-y
+            cur_x = xpos.front();
+            cur_y = ypos.front() - next_step;
+            tempx.push_back(cur_x);
+            tempy.push_back(cur_y);
+            cout<<"after -y "<< cur_x << " "<<cur_y<<endl;
+            if(x==cur_x && y==cur_y)
+                return;
+            //skip
+            tempx.push_back(xpos.front());
+            tempy.push_back(ypos.front());
+            cout<<"after skip "<< xpos.back() << " "<<ypos.back()<<endl;
+            if(x==cur_x && y==cur_y)
+                return;
+            xpos.pop_front();
+            ypos.pop_front();
+        }
+        while(!tempx.empty()){
+            xpos.push_back(tempx.front());
+            ypos.push_back(tempy.front());
+            tempx.pop_front();
+            tempy.pop_front();
+        }
+
+    }
+}
+int heuristic(){
+
+}
+void IDS(){
     
 }
-
 
 int main(){
     fstream file("IntroAI_PR1_test.txt"); 
@@ -29,11 +94,14 @@ int main(){
         cout<<search<<" ";
         cout<<x<<" "<<y<<" ";
 
-        vector<int> steps;
+        deque<int> steps;
         while(ss>>temp){
             steps.push_back(temp);
             cout<<temp<<" ";
         }
+        if(search == "BFS")
+            BFS(steps, x, y);
+        //system("pause");
         cout<<endl;
 
     }
